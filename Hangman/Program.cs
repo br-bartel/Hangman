73a4b299ba4d3e7935c0878381730fb56cdde8e1
unknown <chris.n.gutierrez@gmail.com>
2019-10-11 00:39:04 -0700

    Added method to display user previous user inputs

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -6,13 +6,13 @@ namespace HangmanProject
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 
     class Display
     {
-
+        protected static string InputList = "";
         protected static int origRow;
         protected static int origCol;
 
@@ -31,48 +31,48 @@ namespace HangmanProject
         }
 
         //Hangman's Hangy thing
-        WriteAt("O", 1, 3);
-        WriteAt("O", 1, 4);
-        WriteAt("O",1,5);
-        WriteAt("O",1,6);
-        WriteAt("O",1,7);
-        WriteAt("O",1,8);
-        WriteAt("O",1,9);
-        WriteAt("O",1,10);
-        WriteAt("O",1,11);
-        WriteAt("O",1,12);
-        WriteAt("O",1,13);
-        WriteAt("O",1,14);
-        WriteAt("O",1,15);
-        WriteAt("O",1,16);
-        WriteAt("O",1,17);
-        WriteAt("O",1,18);
-        WriteAt("O",1,19);
-        WriteAt("O",1,20);
-        WriteAt("O",1,21);
-        WriteAt("O",1,22);
-        WriteAt("O",1,23);
-        WriteAt("O",1,24);
-        WriteAt("O",1,25);
-        WriteAt("O",1,26);
-        //base
-        WriteAt("O",0,26);
-        WriteAt("O",2,26);
-        //top thingy
-        WriteAt("O",2,3);
-        WriteAt("O",3,3);
-        WriteAt("O",4,3);
+        //WriteAt("O", 1, 3);
+        //WriteAt("O", 1, 4);
+        //WriteAt("O",1,5);
+        //WriteAt("O",1,6);
+        //WriteAt("O",1,7);
+        //WriteAt("O",1,8);
+        //WriteAt("O",1,9);
+        //WriteAt("O",1,10);
+        //WriteAt("O",1,11);
+        //WriteAt("O",1,12);
+        //WriteAt("O",1,13);
+        //WriteAt("O",1,14);
+        //WriteAt("O",1,15);
+        //WriteAt("O",1,16);
+        //WriteAt("O",1,17);
+        //WriteAt("O",1,18);
+        //WriteAt("O",1,19);
+        //WriteAt("O",1,20);
+        //WriteAt("O",1,21);
+        //WriteAt("O",1,22);
+        //WriteAt("O",1,23);
+        //WriteAt("O",1,24);
+        //WriteAt("O",1,25);
+        //WriteAt("O",1,26);
+        ////base
+        //WriteAt("O",0,26);
+        //WriteAt("O",2,26);
+        ////top thingy
+        //WriteAt("O",2,3);
+        //WriteAt("O",3,3);
+        //WriteAt("O",4,3);
 
-        //head
-        WriteAt("O",3,5);
-        WriteAt("O",3,4);
-        WriteAt("O",3,6);
-        WriteAt("O",4,4);
-        WriteAt("O",4,5);
-        WriteAt("O",4,6);
-        WriteAt("O",5,4);
-        WriteAt("O",5,5);
-        WriteAt("O",5,6);
+        ////head
+        //WriteAt("O",3,5);
+        //WriteAt("O",3,4);
+        //WriteAt("O",3,6);
+        //WriteAt("O",4,4);
+        //WriteAt("O",4,5);
+        //WriteAt("O",4,6);
+        //WriteAt("O",5,4);
+        //WriteAt("O",5,5);
+        //WriteAt("O",5,6);
 
         //body
 
@@ -86,9 +86,23 @@ namespace HangmanProject
         //right leg
 
 
-
-
+        //Takes input and displays in row previous inputs
+        //Place commented code below in Program.Main to demo InputDisplay method
+        //
+        //while (true)
+        //    {
+        //        Display display = new Display();
+        //string input = Console.ReadLine();
+        //Display.InputDisplay(input);
+        //    }
+    public static void InputDisplay(string input)
+        {
+            InputList += input + " ";
+            Console.Clear();
+            Console.Write(InputList);
+        }
     }
+    
 
     class Words
     {
